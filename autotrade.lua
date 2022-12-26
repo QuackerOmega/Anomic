3b335f4eada26dd55c032860ac18d1470cd38e2b
QuackerOmega <98679693+QuackerOmega@users.noreply.github.com>
2022-12-27 00:11:29 +0800

    Update autotrade.lua

--- autotrade.lua
+++ autotrade.lua
@@ -1,16 +1,16 @@
-while task.wait(10) do if not getgenv().autotrade then break end
+for i = 1,AmountToTrade do if not getgenv().autotrade then break end
     for i,v in next, game.Players.LocalPlayer.Backpack:GetChildren() do
-        if v:IsA("Tool") then
-            game.Players.LocalPlayer.character.Humanoid:EquipTool(v)
-            break
-        end
-    end
-    wait(1)
-    local args = {
-        [1] = game:GetService("Players")[PlayerToTrade],
-        [2] = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Tool"),
-        [3] = AmountToPay
-    }
+        if v:IsA("Tool") and v.Name ~= "Boombox" then
+            v.Parent = v.Parent.Parent.Character
+            wait(1)
+            local args = {
+                [1] = game:GetService("Players")[PlayerToTrade],
+                [2] = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Tool"),
+                [3] = 1e5
+            }
 
-    game:GetService("ReplicatedStorage"):FindFirstChild("_CS.Events").TradeSellerBuyer:FireServer(unpack(args))
+            game:GetService("ReplicatedStorage"):FindFirstChild("_CS.Events").TradeSellerBuyer:FireServer(unpack(args))
+        end
+    task.wait(10)
+    end
 end
